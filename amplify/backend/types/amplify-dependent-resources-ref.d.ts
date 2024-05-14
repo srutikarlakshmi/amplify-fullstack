621344e5c6d031e1b57be53ee5320a198845ad7d
Srutika R Lakshmi <srutikarlakshmi@gmail.com>
2024-05-14 19:38:23 +0530

    Enabling Cognito Auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "quizzapp4e39ae7b": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
